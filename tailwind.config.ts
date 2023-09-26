398369d88fe99a9a8ac9e372637fb5b618ee0dca
unknown <tana.palam@gmail.com>
2023-09-26 11:30:12 +0300

    initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
+module.exports = {
+  mode: 'jit',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './sections/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    // MEDIA QUERIES
+    screens: {},
+    //BOX SHADOW
+    boxShadow: {},
+    // THEME
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundImage: {},
+      fontFamily: {},
+      fontSize: {},
+      // BORDER
+      borderRadius: {},
+      // ALL COLORS
+      colors: {},
+
+      // KEYFRAMES
+      keyframes: {
+        side: {},
+      },
+      // ANIMATION
+      animation: {
+        side: 'side 1s ease-in-out infinite',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
