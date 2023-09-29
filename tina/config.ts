2d6ea104910e3dd482f7a5c80b3b74faaf3fbce7
Olga Birkovska <aiba.aki9@gmail.com>
2023-09-29 12:58:35 +0300

    add about collection > general section

--- tina/config.ts
+++ tina/config.ts
@@ -58,6 +58,152 @@ export default defineConfig({
           },
         ],
       },
+      {
+        name: 'about',
+        label: 'About',
+        path: 'content/about',
+        format: 'md',
+        fields: [
+          {
+            name: 'general',
+            label: 'General',
+            type: 'object',
+            required: true,
+            fields: [
+              {
+                name: 'uk',
+                label: 'UK',
+                type: 'object',
+                fields: [
+                  {
+                    name: 'name',
+                    label: 'Name',
+                    type: 'string',
+                  },
+                  {
+                    name: 'profession',
+                    label: 'Profession',
+                    type: 'rich-text',
+                  },
+                  {
+                    name: 'education',
+                    label: 'Education',
+                    type: 'object',
+                    list: true,
+                    fields: [
+                      {
+                        name: 'institution',
+                        label: 'Institution',
+                        type: 'string',
+                      },
+                    ],
+                    ui: {
+                      itemProps: item => {
+                        // if the institution field type is changed to rich-text,
+                        // this part will need re-writing
+                        // as we would be handling a deeper object
+                        return { label: `${item?.institution}` };
+                      },
+                      defaultItem: {
+                        institution: '',
+                      },
+                    },
+                  },
+                  {
+                    name: 'experience',
+                    label: 'Experience',
+                    type: 'object',
+                    list: true,
+                    fields: [
+                      {
+                        name: 'point',
+                        label: 'Point',
+                        type: 'string',
+                      },
+                    ],
+                    ui: {
+                      itemProps: item => {
+                        // if the point field type is changed to rich-text,
+                        // this part will need re-writing
+                        // as we would be handling a deeper object
+                        return { label: `${item?.point}` };
+                      },
+                      defaultItem: {
+                        point: '',
+                      },
+                    },
+                  },
+                ],
+              },
+              {
+                name: 'en',
+                label: 'EN',
+                type: 'object',
+                fields: [
+                  {
+                    name: 'name',
+                    label: 'Name',
+                    type: 'string',
+                  },
+                  {
+                    name: 'profession',
+                    label: 'Profession',
+                    type: 'rich-text',
+                  },
+                  {
+                    name: 'education',
+                    label: 'Education',
+                    type: 'object',
+                    list: true,
+                    fields: [
+                      {
+                        name: 'institution',
+                        label: 'Institution',
+                        type: 'string',
+                      },
+                    ],
+                    ui: {
+                      itemProps: item => {
+                        // if the institution field type is changed to rich-text,
+                        // this part will need re-writing
+                        // as we would be handling a deeper object
+                        return { label: `${item?.institution}` };
+                      },
+                      defaultItem: {
+                        institution: '',
+                      },
+                    },
+                  },
+                  {
+                    name: 'experience',
+                    label: 'Experience',
+                    type: 'object',
+                    list: true,
+                    fields: [
+                      {
+                        name: 'point',
+                        label: 'Point',
+                        type: 'string',
+                      },
+                    ],
+                    ui: {
+                      itemProps: item => {
+                        // if the point field type is changed to rich-text,
+                        // this part will need re-writing
+                        // as we would be handling a deeper object
+                        return { label: `${item?.point}` };
+                      },
+                      defaultItem: {
+                        point: '',
+                      },
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
     ],
   },
   media: {
