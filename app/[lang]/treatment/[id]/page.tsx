7fd70768b61cbc163d9837dc9ec1f8ba32595dc9
Olga Birkovska <aiba.aki9@gmail.com>
2023-10-16 11:03:27 +0300

    add form to dynamic page

--- app/[lang]/treatment/[id]/page.tsx
+++ app/[lang]/treatment/[id]/page.tsx
@@ -1,7 +1,11 @@
 import { Locale } from '@/i18n.config';
 import { getDictionary } from '@/utils/dictionary';
 
-import { ContactsSection, TreatmentDetailsSection } from '@/sections';
+import {
+  ContactsSection,
+  ConsultationSection,
+  TreatmentDetailsSection,
+} from '@/sections';
 
 export default async function Treatment({
   params: { lang, id },
@@ -13,6 +17,10 @@ export default async function Treatment({
   return (
     <main>
       <TreatmentDetailsSection staticData={page.treatment[id]} />
+      <ConsultationSection
+        staticData={page.home.consultation}
+        className="-mt-5 mb-10 md:-mt-8 xl:-mt-10"
+      />
       <ContactsSection
         staticData={{ pageData: page.home, socialData: socials }}
       />
