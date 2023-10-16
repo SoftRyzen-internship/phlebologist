5881416b886f7f9468119eb99727734b65d485a7
yevhen-petrunkin <novamovaxxi@gmail.com>
2023-10-16 14:21:15 +0300

    fix radius in feedback section

--- sections/Feedback/Feedback.tsx
+++ sections/Feedback/Feedback.tsx
@@ -12,7 +12,7 @@ const Feedback: React.FC<FeedbackProps> = async ({ staticData, lang }) => {
       className="-mt-[20px] text-black-dark  md:-mt-[36px] xl:-mt-[48px]"
     >
       <div className="container">
-        <div className="wrapper relative rounded-medium bg-gray-light pb-[32px] pt-[24px] md:rounded-extraLarge md:py-[40px] xl:rounded-max xl:py-[64px]">
+        <div className="wrapper relative rounded-t-medium bg-gray-light pb-[32px] pt-[24px] md:rounded-t-extraLarge md:py-[40px] xl:rounded-t-max xl:py-[64px]">
           <Heading
             variant="primary"
             className="mb-[20px] md:absolute md:left-[24px] md:top-[40px] md:mb-0 xl:top-[64px]"
