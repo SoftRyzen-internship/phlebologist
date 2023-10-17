99b19a2763ca90db9385422eaaea758a9feb8c0f
Olga Birkovska <aiba.aki9@gmail.com>
2023-10-17 09:51:37 +0300

    fix style formatting

--- sections/Treatments/Treatments.tsx
+++ sections/Treatments/Treatments.tsx
@@ -7,12 +7,7 @@ const Treatments: React.FC<TreatmentsProps> = ({ staticData, lang }) => {
   return (
     <section className="xl:mt[-48px] mt-[-20px] md:mt-[-36px]" id="methods">
       <div className="container">
-        <div
-          className="wrapper rounded-t-medium bg-white pb-[52px] pt-6  
-                        md:rounded-t-extraLarge md:pb-[76px] md:pt-[40px] 
-                        xl:relative xl:flex xl:h-[831px]
-                        xl:flex-col xl:flex-wrap xl:rounded-t-max xl:pb-[112px] xl:pt-16"
-        >
+        <div className="wrapper rounded-t-medium bg-white pb-[52px] pt-6 md:rounded-t-extraLarge md:pb-[76px] md:pt-[40px] xl:relative xl:flex xl:h-[831px] xl:flex-col xl:flex-wrap xl:rounded-t-max xl:pb-[112px] xl:pt-16">
           <Heading className="mb-5 md:mb-8 xl:mb-12">{title}</Heading>
 
           <TreatmentTabList lang={lang} data={{ button, more, methods }} />
