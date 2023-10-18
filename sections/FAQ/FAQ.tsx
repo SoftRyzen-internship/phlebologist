f846ac8e3560c42a7f22f978442e97e746fce2c3
Olga Birkovska <aiba.aki9@gmail.com>
2023-10-18 18:30:59 +0300

    fix faq errors & section rounded issue

--- sections/FAQ/FAQ.tsx
+++ sections/FAQ/FAQ.tsx
@@ -10,9 +10,9 @@ const FAQ: React.FC<FAQProps> = ({ staticData: { faq, socials } }) => {
     <section className="xl:mt[-48px] mt-[-20px] md:mt-[-36px]" id="faq">
       <div className="container">
         <div
-          className="wrapper rounded-t-medium bg-white pb-[52px] pt-6  
+          className="wrapper relative rounded-t-medium bg-white pb-[52px] pt-6  
             md:rounded-t-extraLarge md:pb-[76px] md:pt-[40px] 
-            xl:relative xl:flex xl:min-h-[940px] xl:justify-between xl:rounded-t-max xl:pb-[112px] xl:pt-16"
+            xl:flex xl:min-h-[940px] xl:justify-between xl:rounded-t-max xl:pb-[112px] xl:pt-16"
         >
           <Heading className="mb-5 md:mb-8 md:w-[428px] xl:mb-0 xl:w-[360px]">
             {title}
