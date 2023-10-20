d1a64bd871a5e40d16fdb4fc4d463febb9b778a0
i-havr <i.havrylov@gmail.com>
2023-10-20 11:59:27 +0300

    fix template in results section

--- sections/Results/Results.tsx
+++ sections/Results/Results.tsx
@@ -29,7 +29,7 @@ const Results: FC<ResultsProps> = async ({ staticData, iconData, lang }) => {
   return (
     <section id="results" className="-mt-5 md:-mt-8 xl:-mt-10">
       <div className="container">
-        <div className="wrapper rounded-t-medium bg-white pb-[52px] pt-6 md:relative md:rounded-t-extraLarge md:pt-[67px] xl:rounded-t-max xl:pb-[112px] xl:pt-[135px]">
+        <div className="wrapper rounded-t-medium bg-white pb-[52px] pt-6 md:relative md:rounded-t-extraLarge md:pb-[76px] md:pt-[67px] xl:rounded-t-max xl:pb-[112px] xl:pt-[135px]">
           <Heading
             tag="h2"
             className="mb-5 w-[260px] md:absolute md:top-10 md:mb-0 md:w-[500px] xl:top-16 xl:leading-[1]"
