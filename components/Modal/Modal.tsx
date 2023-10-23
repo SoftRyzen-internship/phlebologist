9afe4f72ec010a72df230a3e611d00fc9c324b09
Olga Birkovska <aiba.aki9@gmail.com>
2023-10-23 09:49:33 +0300

    apply margin to modal when zoom > 100%

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -17,7 +17,7 @@ const Modal: React.FC<ModalProps> = ({
 }) => {
   const [isReady, setIsReady] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(isOpen);
-  // const [modalRef, setModalRef] = useState<HTMLElement | null>(null)
+  const [zoom, setZoom] = useState(false);
 
   const transition = useTransition(isModalOpen, {
     from: { opacity: 0 },
@@ -32,15 +32,16 @@ const Modal: React.FC<ModalProps> = ({
 
   useEffect(() => {
     const portalRef = document.getElementById('modal');
-    // const modal = document.getElementById('mod_win');
 
     if (portalRef) {
       setIsReady(true);
     }
 
-    // if (modal) {
-    //   setModalRef(modal)
-    // }
+    const browserZoomLevel = Math.round(window.devicePixelRatio * 100);
+
+    if (browserZoomLevel > 100) {
+      setZoom(true);
+    }
   }, []);
 
   useEffect(() => {
@@ -89,8 +90,7 @@ const Modal: React.FC<ModalProps> = ({
                 className={classNames({
                   'fixed inset-0 z-20 flex items-center justify-center bg-black-backdrop/25 backdrop-blur-sm transition-all duration-300':
                     true,
-                  'overflow-y-scroll md:items-start': isReview,
-                  // 'overflow-y-scroll items-start': (modalRef?.offsetHeight as number > window.innerHeight)
+                  'overflow-y-scroll md:items-start': zoom,
                 })}
                 style={style}
                 onClick={handleBackdropClose}
@@ -99,11 +99,11 @@ const Modal: React.FC<ModalProps> = ({
                   className={classNames({
                     'relative max-w-[280px] overflow-hidden rounded-extended bg-white px-[20px] pb-[40px] pt-[56px] md:max-w-[704px] xl:max-w-[1240px]':
                       true,
-                    'md:mb-auto md:mt-[60px] md:p-[80px]': isReview,
+                    'md:p-[80px]': isReview,
+                    'md:mb-auto md:mt-[60px]': zoom,
                     'md:px-[24px] md:py-[60px] xl:px-[100px] xl:py-[80px] smOnly:w-[80%] smOnly:max-w-[440px]':
                       !isReview,
                   })}
-                  id="mod_win"
                 >
                   <IconBtn
                     variant="close"
