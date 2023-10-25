4cadbf2e758f35d7adbf7a1178edd34ef4437a73
yevhen-petrunkin <novamovaxxi@gmail.com>
2023-10-25 09:51:57 +0300

    setup fetch environment and fisx relavidation

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -1,6 +1,8 @@
 import client from '@/tina/__generated__/client';
 import { NextResponse } from 'next/server';
 
+export const revalidate = 0;
+
 export async function POST(request: Request) {
   try {
     const body: any = await request.json();
